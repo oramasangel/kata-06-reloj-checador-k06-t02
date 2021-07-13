69380c832ffcc731bef78eaf6ef10990aa1feda2
Marcos Gil <marcoswaldo52@gmail.com>
2021-07-13 12:23:38 -0500

    Create model and migration for employee

---
+++ appname/db/migrate/20210713171800_create_employees.rb
@@ -0,0 +1,15 @@
+class CreateEmployees < ActiveRecord::Migration[6.1]
+  def change
+    create_table :employees do |t|
+      t.integer :id
+      t.integer :num_employee
+      t.integer :private_num
+      t.string :name
+      t.string :email
+      t.string :position
+      t.boolean :status
+
+      t.timestamps
+    end
+  end
+end
