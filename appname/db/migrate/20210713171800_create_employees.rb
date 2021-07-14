8b106253066a3dab1729251a37960074ce7bf220
Marcos Gil <marcoswaldo52@gmail.com>
2021-07-14 11:14:29 -0500

    fix error with migation db

--- appname/db/migrate/20210713171800_create_employees.rb
+++ appname/db/migrate/20210713171800_create_employees.rb
@@ -1,7 +1,6 @@
 class CreateEmployees < ActiveRecord::Migration[6.1]
   def change
     create_table :employees do |t|
-      t.integer :id
       t.integer :num_employee
       t.integer :private_num
       t.string :name
