ba298e8f76bcba93990a3e624942fdd334886ed4
Ricardo Jiménez <joserjc.96@gmail.com>
2021-07-20 10:25:08 -0500

    delete trash
    i delete show in json and layout

--- appname/app/controllers/companies_controller.rb
+++ appname/app/controllers/companies_controller.rb
@@ -1,5 +1,5 @@
 class CompaniesController < ApplicationController 
-  layout "company", only: [:edit, :show, :new, :index]
+  layout "company", only: [:edit, :new, :index]
   before_action :set_company, only: %i[ show edit update destroy ]
 
   # GET /companies or /companies.json
@@ -22,7 +22,7 @@ class CompaniesController < ApplicationController
     respond_to do |format|
       if @company.save
         format.html { redirect_to @company, notice: "Company branch was successfully created." }
-        format.json { render :show, status: :created, location: @company }
+        format.json { render status: :created, location: @company }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @company.errors, status: :unprocessable_entity }
@@ -35,7 +35,7 @@ class CompaniesController < ApplicationController
     respond_to do |format|
       if @company.update(company_params)
         format.html { redirect_to @company, notice: "Company branch was successfully updated." }
-        format.json { render :show, status: :ok, location: @company }
+        format.json { render status: :ok, location: @company }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @company.errors, status: :unprocessable_entity }
