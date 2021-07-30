ce5cdd095dd6569422a500d991391c9d8b07a09b
JoseRicardoJimenez <joserjc.96@gmail.com>
2021-07-29 21:48:45 -0500

    i add style and links between all views

--- appname/app/controllers/companies_controller.rb
+++ appname/app/controllers/companies_controller.rb
@@ -5,6 +5,10 @@ class CompaniesController < ApplicationController
   def index
     @companies = Company.all 
   end  
+
+  def show
+  end  
+
   # GET /companies/new
   def new
     @company = Company.new
@@ -21,7 +25,7 @@ class CompaniesController < ApplicationController
     respond_to do |format|
       if @company.save
         format.html { redirect_to @company, notice: "Company branch was successfully created." }
-        format.json { render status: :created, location: @company }
+        format.json { render :show, status: :created, location: @company }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @company.errors, status: :unprocessable_entity }
@@ -34,7 +38,7 @@ class CompaniesController < ApplicationController
     respond_to do |format|
       if @company.update(company_params)
         format.html { redirect_to @company, notice: "Company branch was successfully updated." }
-        format.json { render status: :ok, location: @company }
+        format.json { render :show, status: :ok, location: @company }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @company.errors, status: :unprocessable_entity }
